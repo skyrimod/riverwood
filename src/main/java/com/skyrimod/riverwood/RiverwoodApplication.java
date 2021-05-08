7c22acf3d3a6a00e58b5580503021b6939d912a5
suixin <2421902248@qq.com>
2021-05-08 16:55:30 +0800

    创建项目

---
+++ src/main/java/com/skyrimod/riverwood/RiverwoodApplication.java
@@ -0,0 +1,13 @@
+package com.skyrimod.riverwood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiverwoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiverwoodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skyrimod/riverwood/RiverwoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skyrimod.riverwood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiverwoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
