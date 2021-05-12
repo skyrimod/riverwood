b7b8c5779e0aecc5a59564178861d05ed059deec
suixin <2421902248@qq.com>
2021-05-12 09:06:58 +0800

    add SourceTest类

---
+++ src/test/java/com/skyrimod/riverwood/SourceTest.java
@@ -0,0 +1,20 @@
+package com.skyrimod.riverwood;
+
+import org.junit.Test;
+import sun.tools.java.Scanner;
+
+import java.util.Collections.*;
+import java.util.stream.Stream;
+
+/**
+ * @Classname SourceTest
+ * @Description
+ * @author: suixin
+ * @date: 2021/5/11
+ */
+public class SourceTest {
+    @Test
+    public void test(){
+
+    }
+}
