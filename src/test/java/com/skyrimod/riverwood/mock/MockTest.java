616b1a31c353ef6741e6e5113199fa00b3fcbc5e
jdj <jdj01128739@alibaba-inc.com>
2021-08-12 11:10:53 +0800

    add mockito

--- src/test/java/com/skyrimod/riverwood/mock/MockTest.java
+++ src/test/java/com/skyrimod/riverwood/mock/MockTest.java
@@ -1,10 +1,16 @@
 package com.skyrimod.riverwood.mock;
 
 import com.skyrimod.riverwood.BaseTest;
+import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @ClassName MockTest
@@ -12,9 +18,73 @@ import java.util.ArrayList;
  * @Author jdj
  * @Date 2021/7/29 下午3:23
  **/
-public class MockTest extends BaseTest{
+public class MockTest{
     @Test
-    public void test(){
+    public void test5(){
+        ArrayList<String> list = new ArrayList();
+        list.add("a");
+        list.add("b");
 
+        ArrayList mock = mock(ArrayList.class);
+        mock.addAll(list);
+
+        ArgumentCaptor<ArrayList> captor = ArgumentCaptor.forClass(ArrayList.class);
+        verify(mock).addAll(captor.capture());
+
+        Assert.assertEquals(captor.getValue().size(),2);
+        Assert.assertEquals(captor.getValue(),list);
+    }
+    @Test
+    public void test4(){
+        ArrayList<String> str = new ArrayList<>();
+        ArrayList<String> spy = spy(str);
+
+        spy.add("a");
+        spy.add("b");
+        when(spy.size()).thenReturn(100);
+
+        Assert.assertEquals(spy.get(0),"a");
+        Assert.assertEquals(spy.get(1),"b");
+        Assert.assertEquals(spy.size(),100);
+    }
+    @Test
+    public void test3(){
+        List mockList = mock(List.class);
+
+        mockList.add("one");
+        mockList.add("two");
+        mockList.add("two");
+        mockList.add("three");
+        mockList.add("three");
+        mockList.add("three");
+
+        when(mockList.size()).thenReturn(6);
+        Assert.assertEquals(mockList.size(),6);
+
+        verify(mockList,atLeastOnce()).add("one");
+        verify(mockList,times(2)).add("two");
+        verify(mockList,times(3)).add("three");
+    }
+    @Test
+    public void test2(){
+        Iterator mock = mock(Iterator.class);
+        when(mock.next()).thenReturn("Hello").thenReturn("Mockito!");
+        String result = mock.next() + " " + mock.next();
+        Assert.assertEquals(result,"Hello Mockito!");
+
+        // 类似于环境配置context
+        doThrow(new RuntimeException()).when(mock).next();
+        mock.next();
+    }
+    @Test
+    public void test1(){
+        List mockList = mock(List.class);
+        Assert.assertTrue(mockList instanceof List);
+
+        when(mockList.add("a")).thenReturn(true);
+        Assert.assertTrue(mockList.add("a"));
+
+        doThrow(new RuntimeException()).when(mockList).add("b");
+        mockList.add("b");
     }
 }
